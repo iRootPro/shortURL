1e2f0a3256cb8bd79251f5b0a38a55dfcb7bc28e
Alexander Neupokoev <aleksandr.neupokoev@sbermarket.ru>
2022-10-18 13:00:24 +0300

    fix

--- internal/url/storage/storage.go
+++ internal/url/storage/storage.go
@@ -18,12 +18,15 @@ var Links []LinkEntity
 
 func SaveLinkFile(filename string, newLink LinkEntity) error {
 	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0777)
-	defer file.Close()
 	if err != nil {
 		return fmt.Errorf("file open: %s", err.Error())
 	}
+	defer file.Close()
 
 	fileBuffer, err := io.ReadAll(file)
+	if err != nil {
+		return fmt.Errorf("read from file: %s", err)
+	}
 	if len(fileBuffer) != 0 {
 		if err = json.Unmarshal(fileBuffer, &Links); err != nil {
 			return fmt.Errorf("unmarshaling: %s", err.Error())
@@ -47,10 +50,10 @@ func SaveLinkFile(filename string, newLink LinkEntity) error {
 
 func ShortLinkById(filename string, id string) (string, error) {
 	file, err := os.OpenFile(filename, os.O_RDONLY, 0777)
-	defer file.Close()
 	if err != nil {
 		return "", fmt.Errorf("open file, %s", err.Error())
 	}
+	defer file.Close()
 
 	fileBuff, err := io.ReadAll(file)
 	if err != nil {
